7428efdd8d41705b1c58b5ea467e53be78f14221
Jan Kuri <jkuri88@gmail.com>
2016-09-12 10:46:59 +0200

    bump version

--- ng2-datepicker.d.ts
+++ ng2-datepicker.d.ts
@@ -1,6 +1,1 @@
-import { DatePicker } from './src/components/ng2-datepicker';
-export * from './src/components/ng2-datepicker';
-declare var _default: {
-    directives: typeof DatePicker[];
-};
-export default _default;
+export declare const DatePicker: any[];
