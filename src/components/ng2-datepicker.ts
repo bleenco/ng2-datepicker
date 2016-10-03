3dc289d641eb34a6151b71c62409ac04d7c3899c
Kuba Matjanowski <k.matjanowski@gmail.com>
2016-10-03 09:15:03 +0200

    Added moduleId
    Added ModuleId enables to use relative paths in metadata.
    https://angular.io/docs/ts/latest/cookbook/component-relative-paths.html

--- src/components/ng2-datepicker.ts
+++ src/components/ng2-datepicker.ts
@@ -20,6 +20,7 @@ export const CALENDAR_VALUE_ACCESSOR: any = {
 };
 
 @Component({
+  moduleId: module.id,
   selector: 'datepicker',
   templateUrl: './ng2-datepicker.component.html',
   styleUrls: ['./ng2-datepicker.css'],
