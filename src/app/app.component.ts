fba2cc31712aba2293b85582f8777598adb3420c
Jan Kuri <jkuri88@gmail.com>
2017-03-05 17:58:51 +0100

    chore: fix aot build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { DatePickerOptions, DateModel } from '../../';
+import { DatePickerOptions, DateModel } from '../../ng2-datepicker';
 
 @Component({
   selector: 'app-root',
