c25e63e64c50020c1f56c59295e63375dbf9f23f
Vishal Borker <vishal@cdmx.in>
2017-05-30 09:59:06 +0530

    limit added 120 years

--- src/ng2-datepicker/ng2-datepicker.component.ts
+++ src/ng2-datepicker/ng2-datepicker.component.ts
@@ -322,7 +322,7 @@ export class DatePickerComponent implements ControlValueAccessor, OnInit {
   }
 
   generateYears() {
-    const date: moment.Moment = this.minDate || Moment().year(Moment().year() - 80);
+    const date: moment.Moment = this.minDate || Moment().year(Moment().year() - 120);
     const toDate: moment.Moment = this.maxDate || Moment().year(Moment().year() + 40);
     const years = toDate.year() - date.year();
 
