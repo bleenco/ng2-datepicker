1a8c1ec1dd084a3608509cce0ad6689e8a99fae8
Jan Kuri <jkuri88@gmail.com>
2021-04-05 12:00:20 +0200

    refactor(): init new project

---
+++ projects/ng2-datepicker/src/lib/ng2-datepicker.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-ng2-datepicker',
+  template: `
+    <p>
+      ng2-datepicker works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class Ng2DatepickerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
